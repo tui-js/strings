425a9431178644f5177534fe049e966ae97a387b
Im-Beast <franik.mateusz@gmail.com>
2024-02-12 19:28:04 +0100

    docs(slice): add examples of `slice()` usage

--- src/slice.ts
+++ src/slice.ts
@@ -6,7 +6,14 @@ import { textWidth } from "../mod.ts";
  *
  * @example
  * ```ts
- *
+ *  console.log(slice("Hello, World!", 0, 5)); // "Hello"
+ *  console.log(slice("🐶 woof woof", 0, 7)); // "🐶 woof"
+ *  console.log(slice("🐶", 0, 2)); // "🐶"
+ *  console.log(slice("🐶", 0, 1)); // ""
+ *  console.log(slice("meow 🐱", -2)); // "🐱"
+ *  console.log(slice("\x1b[32mfoo\x1b[0m", 0, 3)); // "\x1b[32mfoo\x1b[0m"
+ *  console.log(slice("\x1b[32mfoo\x1b[0m", 0, 2, true)); // "\x1b[32mfo\x1b[0m" (preserve all ansi styles [default])
+ *  console.log(slice("\x1b[32mfoo\x1b[0m", 0, 2, false)); // "\x1b[32mfo" (preserve only the ansi styles that are in the slice range)
  * ```
  */
 export function slice(
