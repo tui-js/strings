6d6a1267282aa4ea4a562a92ca32b4ad5f82a9d3
Im-Beast <franik.mateusz@gmail.com>
2024-02-13 14:45:11 +0100

    feat(slice): append ellipsis whenever possible
    This makes it easier to predict expected behaviour by making it closer to how `crop` works.

--- src/slice.ts
+++ src/slice.ts
@@ -1,34 +1,48 @@
-import { loopAnsi } from "../mod.ts";
-import { textWidth } from "../mod.ts";
+import { DEFAULT_ELLIPSIS } from "./shared.ts";
+
+import { loopAnsi } from "./ansi_looping.ts";
+import { textWidth } from "./text_width.ts";
 
 /**
  * `String.prototype.slice` but using widths.
+ *
+ * Keep in mind that this function might return string shorter than {end}-{start} in two scenarios:
+ * - {input} text was shorter than specified {end}-{start}.
+ * - {input} text had fullwidth or wide characters which couldn't be fit into {end}-{start}.
+ *  - If that's the case, `slice` will try to append `ellipsis` to the end of the string
+ *    to make it the same width as {end}-{start}.
+ *  - If {ellipsis} itself can't be fit into {end}-{start}, it will be omitted.
+ *
+ * @see {@link DEFAULT_ELLIPSIS} for the default ellipsis character
  *
  * @example
  * ```ts
  *  console.log(slice("Hello, World!", 0, 5)); // "Hello"
  *  console.log(slice("🐶 woof woof", 0, 7)); // "🐶 woof"
  *  console.log(slice("🐶", 0, 2)); // "🐶"
- *  console.log(slice("🐶", 0, 1)); // ""
+ *  console.log(slice("🐶", 0, 1)); // "…"
+ *  console.log(slice("🐶", 0, 1, " ")); // " " (custom ellipsis)
+ *  console.log(slice("🐶", 0, 1, "🐱")); // "" (ellipsis is too wide)
  *  console.log(slice("meow 🐱", -2)); // "🐱"
  *  console.log(slice("\x1b[32mfoo\x1b[0m", 0, 3)); // "\x1b[32mfoo\x1b[0m"
- *  console.log(slice("\x1b[32mfoo\x1b[0m", 0, 2, true)); // "\x1b[32mfo\x1b[0m" (preserve all ansi styles [default])
- *  console.log(slice("\x1b[32mfoo\x1b[0m", 0, 2, false)); // "\x1b[32mfo" (preserve only the ansi styles that are in the slice range)
+ *  console.log(slice("\x1b[32mfoo\x1b[0m", 0, 2, undefined, true)); // "\x1b[32mfo\x1b[0m" (preserve all ansi styles [default])
+ *  console.log(slice("\x1b[32mfoo\x1b[0m", 0, 2, undefined, false)); // "\x1b[32mfo" (preserve only the ansi styles that are in the slice range)
  * ```
  */
 export function slice(
   input: string,
   start?: number,
   end?: number,
+  ellipsis = DEFAULT_ELLIPSIS,
   preserveAllAnsi = true,
 ): string {
   const inputWidth = textWidth(input);
-
   start ??= inputWidth;
   if (start < 0) start = inputWidth + start;
-
   end ??= inputWidth;
   if (end < 0) end = inputWidth + end;
+
+  const ellipsisWidth = textWidth(ellipsis);
 
   let cropped = "";
   let position = 0;
@@ -38,12 +52,20 @@ export function slice(
       return;
     }
 
-    const width = textWidth(char);
+    if (position >= end!) {
+      return !preserveAllAnsi;
+    }
 
-    if (position >= start! && position + width <= end!) {
+    const width = textWidth(char);
+    if (position >= start!) {
+      if (position + width > end!) {
+        if (position + ellipsisWidth === end!) {
+          cropped += ellipsis;
+        }
+        return true;
+      }
+
       cropped += char;
-    } else if (position >= end! && !preserveAllAnsi) {
-      return true;
     }
 
     position += width;
--- tests/slice.test.ts
+++ tests/slice.test.ts
@@ -2,34 +2,37 @@ import { assertEquals } from "jsr:@std/assert@0.215.0";
 import { errorMessage } from "./shared.ts";
 
 import { slice } from "../src/slice.ts";
+import { DEFAULT_ELLIPSIS } from "../src/shared.ts";
 
 Deno.test("slice()", () => {
   const EXPECTED_RESULTS = [
-    ["Hello, World!", 0, 5, undefined, "Hello"],
-    ["🐶 woof woof", 0, 7, undefined, "🐶 woof"],
-    ["🐶", 0, 2, undefined, "🐶"],
-    ["🐶", 0, 1, undefined, ""],
-    ["meow 🐱", -2, undefined, undefined, "🐱"],
-    ["meow 🐱", -1, undefined, undefined, ""],
-    ["meow 🐱", 0, -1, undefined, "meow "],
-    ["meow 🐱", 0, -2, undefined, "meow "],
-    ["meow 🐱", 0, -3, undefined, "meow"],
-    ["こんにちは、世界", 0, 4, undefined, "こん"],
-    ["こんにちは、世界", 0, 5, undefined, "こん"],
-    ["こんにちは、世界", 0, 6, undefined, "こんに"],
-    ["ｱｲｳｴｵ", 0, 1, undefined, "ｱ"],
-    ["ｱｲｳｴｵ", 0, 2, undefined, "ｱｲ"],
-    ["ｱｲｳｴｵ", 0, 3, undefined, "ｱｲｳ"],
-    ["\x1b[32mfoo\x1b[0m", 0, 3, undefined, "\x1b[32mfoo\x1b[0m"],
-    ["\x1b[32mfoo\x1b[0m", 0, 2, true, "\x1b[32mfo\x1b[0m"],
-    ["\x1b[32mfoo\x1b[0m", 0, 2, false, "\x1b[32mfo"],
+    ["Hello, World!", 0, 5, undefined, undefined, "Hello"],
+    ["🐶 woof woof", 0, 7, undefined, undefined, "🐶 woof"],
+    ["🐶", 0, 2, undefined, undefined, "🐶"],
+    ["🐶", 0, 1, undefined, undefined, DEFAULT_ELLIPSIS],
+    ["test hi", -2, undefined, undefined, undefined, "hi"],
+    ["1 meow 🐱", -2, undefined, undefined, undefined, "🐱"],
+    ["2 meow 🐱", -1, undefined, undefined, undefined, ""],
+    ["3 meow 🐱", 0, -1, undefined, undefined, "3 meow " + DEFAULT_ELLIPSIS],
+    ["4 meow 🐱", 0, -2, undefined, undefined, "4 meow "],
+    ["5 meow 🐱", 0, -3, undefined, undefined, "5 meow"],
+    ["こんにちは、世界", 0, 4, undefined, undefined, "こん"],
+    ["こんにちは、世界", 0, 5, undefined, undefined, "こん" + DEFAULT_ELLIPSIS],
+    ["こんにちは、世界", 0, 6, undefined, undefined, "こんに"],
+    ["ｱｲｳｴｵ", 0, 1, undefined, undefined, "ｱ"],
+    ["ｱｲｳｴｵ", 0, 2, undefined, undefined, "ｱｲ"],
+    ["ｱｲｳｴｵ", 0, 3, undefined, undefined, "ｱｲｳ"],
+    ["\x1b[32mfoo\x1b[0m", 0, 3, undefined, undefined, "\x1b[32mfoo\x1b[0m"],
+    ["\x1b[32mfoo\x1b[0m", 0, 2, undefined, true, "\x1b[32mfo\x1b[0m"],
+    ["\x1b[32mfoo\x1b[0m", 0, 2, undefined, false, "\x1b[32mfo"],
   ] as const;
 
   for (
-    const [input, start, end, preserveAllAnsi, expected] of EXPECTED_RESULTS
+    const [input, start, end, ellipsis, preserveAllAnsi, expected]
+      of EXPECTED_RESULTS
   ) {
     assertEquals(
-      slice(input, start, end, preserveAllAnsi),
+      slice(input, start, end, ellipsis, preserveAllAnsi),
       expected,
       errorMessage(input),
     );
